4abde8093afb40d10e6a7f6d4f1be180badd2c6a
Randolph Voorhies <rand.voorhies@gmail.com>
2013-06-14 15:01:06 -0700

    Unit tests for set

--- unittests.cpp
+++ unittests.cpp
@@ -8,6 +8,7 @@
 #include <cereal/binary_archive/string.hpp>
 #include <cereal/binary_archive/map.hpp>
 #include <cereal/binary_archive/queue.hpp>
+#include <cereal/binary_archive/set.hpp>
 #include <limits>
 #include <random>
 
@@ -32,6 +33,12 @@ struct StructBase
   { return x == other.x && y == other.y; }
   bool operator!=(StructBase const & other) const
   { return x != other.x || y != other.y; }
+  bool operator<(StructBase const & other) const
+  { 
+    if (x < other.x) return true;
+    else if(other.x < x) return false;
+    else return (y < other.y);
+  }
 };
 
 std::ostream& operator<<(std::ostream& os, StructBase const & s)
@@ -702,5 +709,65 @@ BOOST_AUTO_TEST_CASE( binary_queue )
     BOOST_CHECK_EQUAL_COLLECTIONS(i_isplqueue_c.begin(),   i_isplqueue_c.end(),   o_isplqueue_c.begin(), o_isplqueue_c.end());
     BOOST_CHECK_EQUAL_COLLECTIONS(i_eserqueue_c.begin(),   i_eserqueue_c.end(),   o_eserqueue_c.begin(), o_eserqueue_c.end());
     BOOST_CHECK_EQUAL_COLLECTIONS(i_esplqueue_c.begin(),   i_esplqueue_c.end(),   o_esplqueue_c.begin(), o_esplqueue_c.end());
+  }
+}
+
+// ######################################################################
+BOOST_AUTO_TEST_CASE( binary_set )
+{
+  std::random_device rd;
+  std::mt19937 gen(rd());
+
+  for(int i=0; i<100; ++i)
+  {
+    std::ostringstream os;
+    cereal::BinaryOutputArchive oar(os);
+
+    std::set<int> o_podset;
+    for(int j=0; j<100; ++j)
+      o_podset.insert(random_value<int>(gen));
+
+    std::set<StructInternalSerialize> o_iserset;
+    for(int j=0; j<100; ++j)
+      o_iserset.insert({ random_value<int>(gen), random_value<int>(gen) });
+
+    std::set<StructInternalSplit> o_isplset;
+    for(int j=0; j<100; ++j)
+      o_isplset.insert({ random_value<int>(gen), random_value<int>(gen) });
+
+    std::set<StructExternalSerialize> o_eserset;
+    for(int j=0; j<100; ++j)
+      o_eserset.insert({ random_value<int>(gen), random_value<int>(gen) });
+
+    std::set<StructExternalSplit> o_esplset;
+    for(int j=0; j<100; ++j)
+      o_esplset.insert({ random_value<int>(gen), random_value<int>(gen) });
+
+    oar & o_podset;
+    oar & o_iserset;
+    oar & o_isplset;
+    oar & o_eserset;
+    oar & o_esplset;
+
+    std::istringstream is(os.str());
+    cereal::BinaryInputArchive iar(is);
+
+    std::set<int> i_podset;
+    std::set<StructInternalSerialize>   i_iserset;
+    std::set<StructInternalSplit>        i_isplset;
+    std::set<StructExternalSerialize> i_eserset;
+    std::set<StructExternalSplit>       i_esplset;
+
+    iar & i_podset;
+    iar & i_iserset;
+    iar & i_isplset;
+    iar & i_eserset;
+    iar & i_esplset;
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_podset.begin(),    i_podset.end(),    o_podset.begin(),  o_podset.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_iserset.begin(),   i_iserset.end(),   o_iserset.begin(), o_iserset.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_isplset.begin(),   i_isplset.end(),   o_isplset.begin(), o_isplset.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_eserset.begin(),   i_eserset.end(),   o_eserset.begin(), o_eserset.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_esplset.begin(),   i_esplset.end(),   o_esplset.begin(), o_esplset.end());
   }
 }
