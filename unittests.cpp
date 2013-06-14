84acf887bbfc2caf5eeda4f1b136bd234b3b0906
Randolph Voorhies <rand.voorhies@gmail.com>
2013-06-14 15:04:04 -0700

    Unit tests for stack

--- unittests.cpp
+++ unittests.cpp
@@ -9,6 +9,7 @@
 #include <cereal/binary_archive/map.hpp>
 #include <cereal/binary_archive/queue.hpp>
 #include <cereal/binary_archive/set.hpp>
+#include <cereal/binary_archive/stack.hpp>
 #include <limits>
 #include <random>
 
@@ -769,5 +770,77 @@ BOOST_AUTO_TEST_CASE( binary_set )
     BOOST_CHECK_EQUAL_COLLECTIONS(i_isplset.begin(),   i_isplset.end(),   o_isplset.begin(), o_isplset.end());
     BOOST_CHECK_EQUAL_COLLECTIONS(i_eserset.begin(),   i_eserset.end(),   o_eserset.begin(), o_eserset.end());
     BOOST_CHECK_EQUAL_COLLECTIONS(i_esplset.begin(),   i_esplset.end(),   o_esplset.begin(), o_esplset.end());
+  }
+}
+
+// ######################################################################
+BOOST_AUTO_TEST_CASE( binary_stack )
+{
+  std::random_device rd;
+  std::mt19937 gen(rd());
+
+  for(int i=0; i<100; ++i)
+  {
+    std::ostringstream os;
+    cereal::BinaryOutputArchive oar(os);
+
+    std::stack<int> o_podstack;
+    for(int j=0; j<100; ++j)
+      o_podstack.push(random_value<int>(gen));
+
+    std::stack<StructInternalSerialize> o_iserstack;
+    for(int j=0; j<100; ++j)
+      o_iserstack.push({ random_value<int>(gen), random_value<int>(gen) });
+
+    std::stack<StructInternalSplit> o_isplstack;
+    for(int j=0; j<100; ++j)
+      o_isplstack.push({ random_value<int>(gen), random_value<int>(gen) });
+
+    std::stack<StructExternalSerialize> o_eserstack;
+    for(int j=0; j<100; ++j)
+      o_eserstack.push({ random_value<int>(gen), random_value<int>(gen) });
+
+    std::stack<StructExternalSplit> o_esplstack;
+    for(int j=0; j<100; ++j)
+      o_esplstack.push({ random_value<int>(gen), random_value<int>(gen) });
+
+    oar & o_podstack;
+    oar & o_iserstack;
+    oar & o_isplstack;
+    oar & o_eserstack;
+    oar & o_esplstack;
+
+    std::istringstream is(os.str());
+    cereal::BinaryInputArchive iar(is);
+
+    std::stack<int> i_podstack;
+    std::stack<StructInternalSerialize> i_iserstack;
+    std::stack<StructInternalSplit>     i_isplstack;
+    std::stack<StructExternalSerialize> i_eserstack;
+    std::stack<StructExternalSplit>     i_esplstack;
+
+    iar & i_podstack;
+    iar & i_iserstack;
+    iar & i_isplstack;
+    iar & i_eserstack;
+    iar & i_esplstack;
+
+    auto & i_podstack_c  = cereal::stack_detail::container(i_podstack);
+    auto & i_iserstack_c = cereal::stack_detail::container(i_iserstack);
+    auto & i_isplstack_c = cereal::stack_detail::container(i_isplstack);
+    auto & i_eserstack_c = cereal::stack_detail::container(i_eserstack);
+    auto & i_esplstack_c = cereal::stack_detail::container(i_esplstack);
+
+    auto & o_podstack_c  = cereal::stack_detail::container(o_podstack);
+    auto & o_iserstack_c = cereal::stack_detail::container(o_iserstack);
+    auto & o_isplstack_c = cereal::stack_detail::container(o_isplstack);
+    auto & o_eserstack_c = cereal::stack_detail::container(o_eserstack);
+    auto & o_esplstack_c = cereal::stack_detail::container(o_esplstack);
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_podstack_c.begin(),    i_podstack_c.end(),    o_podstack_c.begin(),  o_podstack_c.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_iserstack_c.begin(),   i_iserstack_c.end(),   o_iserstack_c.begin(), o_iserstack_c.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_isplstack_c.begin(),   i_isplstack_c.end(),   o_isplstack_c.begin(), o_isplstack_c.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_eserstack_c.begin(),   i_eserstack_c.end(),   o_eserstack_c.begin(), o_eserstack_c.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_esplstack_c.begin(),   i_esplstack_c.end(),   o_esplstack_c.begin(), o_esplstack_c.end());
   }
 }
