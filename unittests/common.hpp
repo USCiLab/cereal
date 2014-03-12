98959ab40a2c1254a19f2ad257ee3d4fbc5ab0ef
Shane Grant <w.shane.grant@gmail.com>
2014-03-11 23:33:28 -0700

    Fixing MSVC unit testing for new format

--- unittests/common.hpp
+++ unittests/common.hpp
@@ -24,6 +24,9 @@
   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+#ifndef CEREAL_TEST_COMMON_H_
+#define CEREAL_TEST_COMMON_H_
+
 #include <cereal/types/memory.hpp>
 #include <cereal/types/array.hpp>
 #include <cereal/types/vector.hpp>
@@ -53,7 +56,7 @@
 
 namespace boost
 {
-  template<class F, class S>
+  template<class F, class S> inline
   ::std::ostream & operator<<(::std::ostream & os, ::std::pair<F, S> const & p)
   {
     os << "([" << p.first << "], [" << p.second << "])";
@@ -64,27 +67,27 @@ namespace boost
 
 namespace cereal
 {
-  template <class Archive, class T>
+  template <class Archive, class T> inline
     void serialize( Archive &, std::less<T> & )
     { }
 }
 
-template<class T>
+template<class T> inline
 typename std::enable_if<std::is_floating_point<T>::value, T>::type
 random_value(std::mt19937 & gen)
 { return std::uniform_real_distribution<T>(-10000.0, 10000.0)(gen); }
 
-template<class T>
+template<class T> inline
 typename std::enable_if<std::is_integral<T>::value && sizeof(T) != sizeof(char), T>::type
 random_value(std::mt19937 & gen)
 { return std::uniform_int_distribution<T>(std::numeric_limits<T>::lowest(), std::numeric_limits<T>::max())(gen); }
 
-template<class T>
+template<class T> inline
 typename std::enable_if<std::is_integral<T>::value && sizeof(T) == sizeof(char), T>::type
 random_value(std::mt19937 & gen)
 { return static_cast<T>( std::uniform_int_distribution<int64_t>(std::numeric_limits<T>::lowest(), std::numeric_limits<T>::max())(gen) ); }
 
-template<class T>
+template<class T> inline
 typename std::enable_if<std::is_same<T, std::string>::value, std::string>::type
 random_value(std::mt19937 & gen)
 {
@@ -94,7 +97,7 @@ random_value(std::mt19937 & gen)
   return s;
 }
 
-template<class C>
+template<class C> inline
 std::basic_string<C> random_basic_string(std::mt19937 & gen)
 {
   std::basic_string<C> s(std::uniform_int_distribution<int>(3, 30)(gen), ' ');
@@ -103,7 +106,7 @@ std::basic_string<C> random_basic_string(std::mt19937 & gen)
   return s;
 }
 
-template <size_t N>
+template <size_t N> inline
 std::string random_binary_string(std::mt19937 & gen)
 {
   std::string s(N, ' ');
@@ -129,7 +132,7 @@ struct StructBase
   }
 };
 
-std::ostream& operator<<(std::ostream& os, StructBase const & s)
+inline std::ostream& operator<<(std::ostream& os, StructBase const & s)
 {
     os << "[x: " << s.x << " y: " << s.y << "]";
     return os;
@@ -181,13 +184,13 @@ struct StructExternalSplit : StructBase
   StructExternalSplit(int x_, int y_) : StructBase{x_,y_} {}
 };
 
-  template<class Archive>
+  template<class Archive> inline
 void save(Archive & ar, StructExternalSplit const & s)
 {
   ar(s.x, s.y);
 }
 
-  template<class Archive>
+  template<class Archive> inline
 void load(Archive & ar, StructExternalSplit & s)
 {
   ar(s.x, s.y);
@@ -204,3 +207,5 @@ struct StructHash {
       return h1 ^ ( h2 << 1 );
     }
 };
+
+#endif // CEREAL_TEST_COMMON_H_
