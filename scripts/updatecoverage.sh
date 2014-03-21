5d31ec2ee1a51d06f530e54fb200e2d73a039c8a
Shane Grant <w.shane.grant@gmail.com>
2014-03-20 22:20:42 -0700

    updating coverage scripts and build

--- scripts/updatecoverage.sh
+++ scripts/updatecoverage.sh
@@ -2,14 +2,32 @@
 
 # Updates the coverage documentation, and copies it into the appropriate place
 # in the gh-pages branch.
+#   $1 from CMAKE will contain the root directory of cereal
+
+# this requires lcov 1.10 or newer
 
 set -e
+pwd
+echo $1
 
+COVERAGE_TESTS=./coverage_*
+
+# run tests
+for f in $COVERAGE_TESTS
+  do
+    echo $f
+    $f
+  done
+
+# build coverage output
 tempdir=`mktemp -d`
 
-make coverage COVERAGE_OUTPUT=${tempdir}/coverage
+lcov --capture --directory $1 --output-file coverage.info --no-external
+lcov --remove coverage.info '*/external/*' 'cereal/details/util.hpp' 'sandbox/*' 'unittests/*' -o coverage.info
+genhtml --demangle-cpp coverage.info --output-directory ${tempdir}
 
-git checkout gh-pages
+# copy over to gh pages
+git checkout gh-pages-1.0
 
 rm -rf assets/coverage
 
