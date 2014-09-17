bbbfda065b375eaa1468b86c037ef1c7f5c60e88
Steve Hickman <steve.hickman@honeywell.com>
2014-09-16 19:42:54 -0700

    boost_optional:
    1) Added unit tests for boost_optional support
    2) Added unit test to unittest project
    3) alphabetized type includes in common.hpp (so it's easier to tell what is being included)
    
    NOTE: project includes reference to boost libraries you may not want to merge in

--- unittests/common.hpp
+++ unittests/common.hpp
@@ -27,25 +27,27 @@
 #ifndef CEREAL_TEST_COMMON_H_
 #define CEREAL_TEST_COMMON_H_
 
-#include <cereal/types/memory.hpp>
 #include <cereal/types/array.hpp>
-#include <cereal/types/vector.hpp>
+#include <cereal/types/bitset.hpp>
+#include <cereal/types/boost_optional.hpp>
+#include <cereal/types/boost_variant.hpp>
+#include <cereal/types/complex.hpp>
+#include <cereal/types/chrono.hpp>
 #include <cereal/types/deque.hpp>
 #include <cereal/types/forward_list.hpp>
 #include <cereal/types/list.hpp>
-#include <cereal/types/string.hpp>
-#include <cereal/types/map.hpp>
 #include <cereal/types/queue.hpp>
+#include <cereal/types/map.hpp>
+#include <cereal/types/memory.hpp>
+#include <cereal/types/polymorphic.hpp>
 #include <cereal/types/set.hpp>
 #include <cereal/types/stack.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/tuple.hpp>
 #include <cereal/types/unordered_map.hpp>
 #include <cereal/types/unordered_set.hpp>
 #include <cereal/types/utility.hpp>
-#include <cereal/types/tuple.hpp>
-#include <cereal/types/bitset.hpp>
-#include <cereal/types/complex.hpp>
-#include <cereal/types/chrono.hpp>
-#include <cereal/types/polymorphic.hpp>
+#include <cereal/types/vector.hpp>
 
 #include <cereal/archives/binary.hpp>
 #include <cereal/archives/portable_binary.hpp>
