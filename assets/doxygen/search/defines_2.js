9fea78f2aad699c881a627b1730d7149daa7d867
Shane Grant <w.shane.grant@gmail.com>
2015-01-19 12:23:26 -0800

    update doxygen for 1.1

---
+++ assets/doxygen/search/defines_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['unregistered_5fpolymorphic_5fexception',['UNREGISTERED_POLYMORPHIC_EXCEPTION',['../polymorphic_8hpp.html#a6a90af3fca239017978477a753225101',1,'polymorphic.hpp']]]
+];
