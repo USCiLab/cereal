1d50761ece412c92f473953e7391bc027b76a126
Shane Grant <w.shane.grant@gmail.com>
2015-12-21 23:21:11 -0800

    Boost test related fix, fix unit test in response to #188

--- unittests/common.hpp
+++ unittests/common.hpp
@@ -55,8 +55,25 @@
 #include <limits>
 #include <random>
 
+#include <boost/test/tools/detail/print_helper.hpp>
+
 namespace boost
 {
+  namespace test_tools
+  {
+    namespace tt_detail
+    {
+      template <class F, class S>
+      struct print_log_value< ::std::pair<F, S> >
+      {
+        void operator()(::std::ostream & os, ::std::pair<F, S> const & p )
+        {
+          os << "([" << p.first << "], [" << p.second << "])";
+        }
+      };
+    }
+  }
+
   template<class F, class S> inline
   ::std::ostream & operator<<(::std::ostream & os, ::std::pair<F, S> const & p)
   {
@@ -64,7 +81,6 @@ namespace boost
     return os;
   }
 }
-
 
 namespace cereal
 {
