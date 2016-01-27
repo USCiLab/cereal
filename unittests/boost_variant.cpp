27e4172f0b24cd7be838ca57304c846246380f86
Shane Grant <w.shane.grant@gmail.com>
2016-01-27 15:10:44 -0800

    Improve variant test relates #230

--- unittests/boost_variant.cpp
+++ unittests/boost_variant.cpp
@@ -30,25 +30,38 @@
 template <class IArchive, class OArchive>
 void test_boost_variant()
 {
-  boost::variant<int> o_bv;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+
+  boost::variant<int, double, std::string> o_bv1 = random_value<int>(gen);
+  boost::variant<int, double, std::string> o_bv2 = random_value<double>(gen);
+  boost::variant<int, double, std::string> o_bv3 = random_basic_string<char>(gen);
 
   std::ostringstream os;
   {
     OArchive oar(os);
 
-    oar(o_bv);
+    oar(o_bv1);
+    oar(o_bv2);
+    oar(o_bv3);
   }
 
-  decltype( o_bv   ) i_bv;
+  decltype(o_bv1) i_bv1;
+  decltype(o_bv2) i_bv2;
+  decltype(o_bv3) i_bv3;
 
   std::istringstream is(os.str());
   {
     IArchive iar(is);
 
-    iar(i_bv);
+    iar(i_bv1);
+    iar(i_bv2);
+    iar(i_bv3);
   }
 
-  BOOST_CHECK_EQUAL( i_bv   == o_bv,   true );
+  BOOST_CHECK_EQUAL( boost::get<int>(i_bv1), boost::get<int>(o_bv1) );
+  BOOST_CHECK_EQUAL( boost::get<double>(i_bv2), boost::get<double>(o_bv2) );
+  BOOST_CHECK_EQUAL( boost::get<std::string>(i_bv3), boost::get<std::string>(o_bv3) );
 }
 
 BOOST_AUTO_TEST_CASE( binary_boost_variant )
