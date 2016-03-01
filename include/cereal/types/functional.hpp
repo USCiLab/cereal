7ea8da1a27581049570ff6002ff77331ea348acf
Shane Grant <w.shane.grant@gmail.com>
2016-03-01 14:31:17 -0800

    update copyright date and remove extra include

--- include/cereal/types/functional.hpp
+++ include/cereal/types/functional.hpp
@@ -2,7 +2,7 @@
     \brief Support for types found in \<functional\>
     \ingroup STLSupport */
 /*
-  Copyright (c) 2014, Randolph Voorhies, Shane Grant
+  Copyright (c) 2016, Randolph Voorhies, Shane Grant
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
@@ -30,7 +30,6 @@
 #ifndef CEREAL_TYPES_FUNCTIONAL_HPP_
 #define CEREAL_TYPES_FUNCTIONAL_HPP_
 
-#include <cereal/details/helpers.hpp>
 #include <functional>
 
 namespace cereal
