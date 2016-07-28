a2c2130c5e515eb83e39143cbd66b621f4bdc740
Christopher Bläsius <christopher.blaesius@krumedia.com>
2016-07-28 15:25:38 +0200

    unittest for xml archive with different root node

---
+++ unittests/xml_archive.cpp
@@ -0,0 +1,25 @@
+#include "common.hpp"
+#include <boost/test/unit_test.hpp>
+
+
+
+// Tests the default behavior to swap bytes to current machine's endianness
+BOOST_AUTO_TEST_CASE( xml_rootnodename )
+{ 
+
+    std::ostringstream os;
+    {
+      cereal::XMLOutputArchive oar(os, cereal::XMLOutputArchive::Options(std::numeric_limits<double>::max_digits10, true, false, "xmlroot");
+      int32_t i_int32 = 42;
+      oar(CEREAL_NVP(i_int32));
+    }
+    int32_t  i_int32  = 0;
+
+    std::istringstream is(os.str());
+    {
+      cereal::XMLInputArchive iar(is, "xmlroot");
+      iar(i_int32);
+    }
+    BOOST_CHECK_EQUAL(i_int32, 42);
+  }
+}
