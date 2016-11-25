2e96b9915e31fc2b90596e9df597e8fa0f930dee
David Hirvonen <dhirvonen@elucideye.com>
2016-11-25 16:02:00 -0500

    Add is_loading and is_saving values to cereal::{Input,Output}Archive
    This provides compatibility for asymmetric serialize routines when transitioning from boost serialization and associated archive types.
    
    See: https://github.com/USCiLab/cereal/issues/360
    
    Example:
    ```
        template <typename Archive>
        void serialize(Archive &ar, const std::uint32_t version)
        {
            std::int16_t tmp = foo;
            if(Archive::is_loading::value)
            {
                ar & tmp;
                foo = tmp; // For illustration only
            }
            else
            {
                tmp = foo; // For illustration only
                ar & tmp;
            }
            ar & foo;
        }
    ```

--- include/cereal/cereal.hpp
+++ include/cereal/cereal.hpp
@@ -255,6 +255,20 @@ namespace cereal
           a large project from Boost to cereal.  The preferred interface for cereal is using operator(). */
       //! @{
 
+      //! Indicates this archive is not intended for loading
+      /*! This ensures compatibility with boost archive types.  If you are transitioning
+          from boost, you can check this value within a member or external serialize function
+          (i.e., Archive::is_loading::value) to disable behavior specific to loading, until 
+          you can transition to split save/load or save_minimal/load_minimal functions */
+      using is_loading = std::false_type;
+
+      //! Indicates this archive is intended for saving
+      /*! This ensures compatibility with boost archive types.  If you are transitioning
+          from boost, you can check this value within a member or external serialize function
+          (i.e., Archive::is_saving::value) to enable behavior specific to loading, until 
+          you can transition to split save/load or save_minimal/load_minimal functions */
+      using is_saving = std::true_type;
+
       //! Serializes passed in data
       /*! This is a boost compatability layer and is not the preferred way of using
           cereal.  If you are transitioning from boost, use this until you can
@@ -610,6 +624,20 @@ namespace cereal
           Functionality that mirrors the syntax for Boost.  This is useful if you are transitioning
           a large project from Boost to cereal.  The preferred interface for cereal is using operator(). */
       //! @{
+
+      //! Indicates this archive is intended for loading
+      /*! This ensures compatibility with boost archive types.  If you are transitioning
+          from boost, you can check this value within a member or external serialize function
+          (i.e., Archive::is_loading::value) to enable behavior specific to loading, until 
+          you can transition to split save/load or save_minimal/load_minimal functions */
+.      using is_loading = std::true_type;
+
+      //! Indicates this archive is not intended for saving
+      /*! This ensures compatibility with boost archive types.  If you are transitioning
+          from boost, you can check this value within a member or external serialize function
+          (i.e., Archive::is_saving::value) to disable behavior specific to loading, until 
+          you can transition to split save/load or save_minimal/load_minimal functions */
+      using is_saving = std::false_type;
 
       //! Serializes passed in data
       /*! This is a boost compatability layer and is not the preferred way of using
