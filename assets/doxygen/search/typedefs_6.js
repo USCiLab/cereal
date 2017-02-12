b3d4bc93fa44e48d977f739cea6526895c4af774
Shane Grant <w.shane.grant@gmail.com>
2017-02-12 13:47:01 -0800

    new dox files apparently

---
+++ assets/doxygen/search/typedefs_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['uniqueserializer',['UniqueSerializer',['../structcereal_1_1detail_1_1InputBindingMap.html#a429d50a04386c8de48e99723564bfbeb',1,'cereal::detail::InputBindingMap']]],
+  ['unused',['unused',['../structcereal_1_1detail_1_1polymorphic__serialization__support.html#af32ebd1e93fee3dbed819bf1cfbfd073',1,'cereal::detail::polymorphic_serialization_support']]]
+];
