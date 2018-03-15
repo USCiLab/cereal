dc9e573dfb907db34c04ad25ef738f0569787a2f
Patrik Huber <patrikhuber@users.noreply.github.com>
2018-03-15 11:36:26 +0000

    Changed serialised index type of std::variant to std::uint32_t
    This makes saving & loading std::variant cross-platform.

--- include/cereal/types/variant.hpp
+++ include/cereal/types/variant.hpp
@@ -32,6 +32,7 @@
 
 #include "cereal/cereal.hpp"
 #include <variant>
+#include <cstdint>
 
 namespace cereal
 {
@@ -80,7 +81,7 @@ namespace cereal
   template <class Archive, typename VariantType1, typename... VariantTypes> inline
   void CEREAL_SAVE_FUNCTION_NAME( Archive & ar, std::variant<VariantType1, VariantTypes...> const & variant )
   {
-    auto index = variant.index();
+    std::uint32_t index = variant.index();
     ar( CEREAL_NVP_("index", index) );
     variant_detail::variant_save_visitor<Archive> visitor(ar);
     std::visit(visitor, variant);
@@ -92,7 +93,7 @@ namespace cereal
   {
     using variant_t = typename std::variant<VariantTypes...>;
 
-    decltype(variant.index()) index;
+    std::uint32_t index;
     ar( CEREAL_NVP_("index", index) );
     if(index >= std::variant_size_v<variant_t>)
       throw Exception("Invalid 'index' selector when deserializing std::variant");
