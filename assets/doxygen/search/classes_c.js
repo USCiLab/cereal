9215e9086223c4d3bd96fe3b529c39dedcc922fd
Shane Grant <w.shane.grant@gmail.com>
2019-10-16 23:21:57 -0700

    Doc update

--- assets/doxygen/search/classes_c.js
+++ assets/doxygen/search/classes_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['options',['Options',['../classcereal_1_1JSONOutputArchive_1_1Options.html',1,'cereal::JSONOutputArchive::Options'],['../classcereal_1_1PortableBinaryOutputArchive_1_1Options.html',1,'cereal::PortableBinaryOutputArchive::Options'],['../classcereal_1_1XMLOutputArchive_1_1Options.html',1,'cereal::XMLOutputArchive::Options'],['../classcereal_1_1PortableBinaryInputArchive_1_1Options.html',1,'cereal::PortableBinaryInputArchive::Options']]],
+  ['options',['Options',['../classcereal_1_1XMLOutputArchive_1_1Options.html',1,'cereal::XMLOutputArchive::Options'],['../classcereal_1_1JSONOutputArchive_1_1Options.html',1,'cereal::JSONOutputArchive::Options'],['../classcereal_1_1PortableBinaryInputArchive_1_1Options.html',1,'cereal::PortableBinaryInputArchive::Options'],['../classcereal_1_1PortableBinaryOutputArchive_1_1Options.html',1,'cereal::PortableBinaryOutputArchive::Options']]],
   ['outputarchive',['OutputArchive',['../classcereal_1_1OutputArchive.html',1,'cereal']]],
   ['outputarchive_3c_20binaryoutputarchive_2c_20allowemptyclasselision_20_3e',['OutputArchive&lt; BinaryOutputArchive, AllowEmptyClassElision &gt;',['../classcereal_1_1OutputArchive.html',1,'cereal']]],
   ['outputarchive_3c_20jsonoutputarchive_20_3e',['OutputArchive&lt; JSONOutputArchive &gt;',['../classcereal_1_1OutputArchive.html',1,'cereal']]],
