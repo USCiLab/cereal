8bd726281f6679cc4ea2ae4cf9e5b8c0349737be
TankorSmash <tankorsmash+github@gmail.com>
2019-11-12 16:29:18 -0500

    Fix typo in docs

--- include/cereal/macros.hpp
+++ include/cereal/macros.hpp
@@ -51,7 +51,7 @@
 
     Note that even with this enabled you must still ensure that
     archives are accessed by only one thread at a time; it is safe
-    to use multiple archives in paralel, but not to access one archive
+    to use multiple archives in parallel, but not to access one archive
     from many places simultaneously. */
 #define CEREAL_THREAD_SAFE 0
 #endif // CEREAL_THREAD_SAFE
