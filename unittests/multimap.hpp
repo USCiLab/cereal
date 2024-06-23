2c647d87d492bea4400e95cf71c2de7270e2f3ab
psykose <alice@ayaya.dev>
2024-06-23 08:21:42 +0200

    fix building tests when char is unsigned
    when char is unsigned it's uint8_t which doesn't match the type declared
    for the key of the map, so it fails with
    
    error: non-constant-expression cannot be narrowed from type
    'typename std::enable_if<std::is_integral<char>::value && sizeof(char) == sizeof(char), char>::type'
    (aka 'char') to 'const signed char' in initializer list
    
    etc

--- unittests/multimap.hpp
+++ unittests/multimap.hpp
@@ -71,7 +71,7 @@ void test_multimap()
     std::multimap<int8_t, StructExternalSplit> o_esplmultimap;
     for(int j=0; j<100; ++j)
     {
-      auto key = random_value<char>(gen);
+      auto key = random_value<int8_t>(gen);
       o_esplmultimap.insert({key,  { random_value<int>(gen), random_value<int>(gen) }});
       o_esplmultimap.insert({key,  { random_value<int>(gen), random_value<int>(gen) }});
     }
